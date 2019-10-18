04f5dc033b69c4446aeb937e83d5292f686a4fdd
troublor <troublor@live.com>
2019-10-18 13:12:33 +0800

    fix: installation permission bugs

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ CONFIG_NAME="gotrash-config.json"
 
 if [[ ! -d ${GOTRASH_PATH} ]]; then
     sudo mkdir -p ${GOTRASH_PATH}
+    sudo chmod 777 -R ${GOTRASH_PATH}
 fi
 if [[ -f ${BIN_PATH}/${CMD_NAME} ]]; then
     sudo rm ${BIN_PATH}/${CMD_NAME}
@@ -17,7 +18,7 @@ if [[ -f ${BIN_PATH}/${CONFIG_NAME} ]]; then
     sudo rm ${BIN_PATH}/${CONFIG_NAME}
 fi
 
-echo "{\"trashDir\":\"${GOTRASH_PATH}\"}" > ${GOTRASH_PATH}/${CONFIG_NAME}
+sudo echo "{\"trashDir\":\"${GOTRASH_PATH}\"}" > ${GOTRASH_PATH}/${CONFIG_NAME}
 go build -o ${GOTRASH_PATH}/${CMD_NAME}
 sudo ln -s ${GOTRASH_PATH}/${CMD_NAME} ${BIN_PATH}/${CMD_NAME}
 sudo chmod 777 ${BIN_PATH}/${CMD_NAME}
